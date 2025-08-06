0cbf74e1b387bfe66ecb23cae81dc01be8d84eee
Shivani <shivanimandhania@gmail.com>
2025-08-06 19:54:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+import Blogs from './components/Blogs'
+import Pagination from './components/Pagination'
+
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <Blogs/>
+      <Pagination/>
+    </div>
+  )
+}
